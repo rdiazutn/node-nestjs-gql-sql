7f092a7342f90948ad71eaa03935f937e948f675
Rodrigo Diaz <rodrigodiaz@frba.utn.edu.ar>
2023-06-30 22:14:22 -0300

    partial commit

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import { User } from './models/User'
 import { UserResolver } from './resolvers/UserResolver'
 import { Salesman } from './models/Salesman'
 import { Branch } from './models/Branch'
-import AddQuery from './middlewares/AddQuery'
 import BranchResolver from './resolvers/BranchResolver'
 
 async function main(){
@@ -16,10 +15,14 @@ async function main(){
     // TODO DELETE THIS
     console.log('Inserting dummy data')
     // --
-    const user = new User()
-    user.username = 'Timber'
-    user.password = 'Saw'
-    await AppDataSource.manager.save(user)
+    User.find().then((users) => {
+      if (users.length === 0 ){
+        const user = new User()
+        user.username = 'rdiaz'
+        user.password = '1234'
+        user.save()
+      }
+    })
     // --
     const branch = new Branch()
     branch.code = '001'
@@ -39,7 +42,6 @@ async function main(){
   // TODO: autoimport from resolvers
   const schema = await buildSchema({
     resolvers: [UserResolver, BranchResolver],
-    globalMiddlewares: [AddQuery]
   })
 
   const server = new ApolloServer({
