2dcbe3c0f02feaa6c1e165ccf402503b97043021
Rodrigo Diaz <rodrigodiaz@frba.utn.edu.ar>
2023-07-16 23:44:48 -0300

    Made class validator work

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import * as dotenv from 'dotenv'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common'
 
 dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT)
 }
 bootstrap()
