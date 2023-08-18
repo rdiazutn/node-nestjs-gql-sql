4eb6ac38e16d01d9098d9a710209559d725e7656
Rodrigo Diaz <rodrigodiaz@frba.utn.edu.ar>
2023-08-18 10:22:14 -0300

    removed ;

--- src/users/models/User.entity.ts
+++ src/users/models/User.entity.ts
@@ -4,9 +4,9 @@ import {
   Column,
   BaseEntity,
   BeforeInsert,
-} from 'typeorm';
-import { ObjectType, Field, ID, InputType } from '@nestjs/graphql';
-import * as bcrypt from 'bcrypt';
+} from 'typeorm'
+import { ObjectType, Field, ID, InputType } from '@nestjs/graphql'
+import * as bcrypt from 'bcrypt'
 // import { Salesman } from '../../sales/models/Salesman'
 
 @Entity()
@@ -14,21 +14,21 @@ import * as bcrypt from 'bcrypt';
 export class User extends BaseEntity {
   @Field(() => ID)
   @PrimaryGeneratedColumn()
-  id: string;
+  id: string
 
   @Field(() => String)
   @Column()
-  username: string;
+  username: string
 
   @Field(() => String)
   @Column({ default: 'USER' })
-  role: string;
+  role: string
 
   @Column()
-  password: string;
+  password: string
 
   @Column({ nullable: true })
-  token: string;
+  token: string
 
   // @OneToOne(() => Salesman, salesman => salesman.user)
   // @Field(() => Salesman)
@@ -36,22 +36,22 @@ export class User extends BaseEntity {
 
   @BeforeInsert()
   async beforeInsert() {
-    const salt = await bcrypt.genSalt(10);
-    this.password = bcrypt.hashSync(this.password, salt);
+    const salt = await bcrypt.genSalt(10)
+    this.password = bcrypt.hashSync(this.password, salt)
   }
 
   toString() {
-    delete this.password;
-    delete this.token;
-    return JSON.stringify(this);
+    delete this.password
+    delete this.token
+    return JSON.stringify(this)
   }
 }
 
 @InputType()
 export class LoginInput {
   @Field(() => String)
-  username: string;
+  username: string
 
   @Field(() => String)
-  password: string;
+  password: string
 }
